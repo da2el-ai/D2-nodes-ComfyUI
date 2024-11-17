13ddef8d337002b7fdaef0e172ddae8e9880542c
da2el-ai <hirosyg.picasa@gmail.com>
2024-11-17 20:30:44 +0900

    fix: D2_GetImageSize: add javascript

---
+++ web/D2_GetImageSize.js
@@ -0,0 +1,36 @@
+import { app } from "/scripts/app.js";
+// import { ComfyWidgets } from "/scripts/widgets.js";
+import { findWidgetByName } from "./utils.js";
+
+app.registerExtension({
+  name: "Comfy.D2.D2_GetImageSize",
+  async beforeRegisterNodeDef(nodeType, nodeData, app) {
+    if (nodeData.name !== "D2 Get Image Size") return;
+
+    const origOnNodeCreated = nodeType.prototype.onNodeCreated;
+    nodeType.prototype.onNodeCreated = function () {
+        const r = origOnNodeCreated ? origOnNodeCreated.apply(this) : undefined;
+        const displayWidget = findWidgetByName(this, "display");
+        displayWidget.inputEl.readOnly = true;
+        displayWidget.inputEl.style.opacity = 0.6;
+        displayWidget.inputEl.style.height = "3em";
+        return r;
+    };
+
+    /**
+     * ノード実行時
+     */
+    const onExecuted = nodeType.prototype.onExecuted;
+    nodeType.prototype.onExecuted = function (message) {
+      onExecuted?.apply(this, arguments);
+
+      console.log("/////////////////", message);
+
+      const w = message["width"][0];
+      const h = message["height"][0];
+
+      const displayWidget = findWidgetByName(this, "display");
+      displayWidget.value = `width: ${w}\nheight: ${h}`;
+    };
+  },
+});
