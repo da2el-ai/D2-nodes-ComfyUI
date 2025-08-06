98d68c1881accfbd67a66b44b6a7e44e4b20896f
da2el <hirosyg.picasa@gmail.com>
2025-08-06 22:57:08 +0900

    style: D2_ImageStack: fix formatting and indentation

--- web/D2_ImageStack.js
+++ web/D2_ImageStack.js
@@ -2,44 +2,42 @@ import { app } from "/scripts/app.js";
 import { findWidgetByName, handleInputsVisibility } from "./modules/utils.js";
 
 const changeInputVisibility = (node, val) => {
-  if (node.constructor.title == "D2 Image Mask Stack"){
-    handleInputsVisibility(node, val, [
-      {name:"image", type:"IMAGE"},
-      {name:"mask", type:"MASK"},
-    ])
-  }else{
-    handleInputsVisibility(node, val, [
-      {name:"image", type:"IMAGE"},
-    ])
-  }
+    if (node.constructor.title == "D2 Image Mask Stack") {
+        handleInputsVisibility(node, val, [
+            { name: "image", type: "IMAGE" },
+            { name: "mask", type: "MASK" },
+        ]);
+    } else {
+        handleInputsVisibility(node, val, [{ name: "image", type: "IMAGE" }]);
+    }
 };
 
 ///////////////////////////
 ///////////////////////////
 app.registerExtension({
-  name: "Comfy.D2.D2_ImageStack",
+    name: "Comfy.D2.D2_ImageStack",
 
-  nodeCreated(node) {
-    if (node.constructor.title == "D2 Image Stack" || node.constructor.title == "D2 Image Mask Stack"){
-      if (node.widgets){
-        const countWidget = findWidgetByName(node, "image_count");
-        let widgetValue = countWidget.value;
+    nodeCreated(node) {
+        if (node.constructor.title == "D2 Image Stack" || node.constructor.title == "D2 Image Mask Stack") {
+            if (node.widgets) {
+                const countWidget = findWidgetByName(node, "image_count");
+                let widgetValue = countWidget.value;
 
-        changeInputVisibility(node, widgetValue);
+                changeInputVisibility(node, widgetValue);
 
-        // lora_count.value の再定義
-        Object.defineProperty(countWidget, 'value', {
-          get() {
-            return widgetValue;
-          },
-          set(newVal) {
-            if (newVal !== widgetValue) {
-              widgetValue = newVal;
-              changeInputVisibility(node, newVal);
+                // lora_count.value の再定義
+                Object.defineProperty(countWidget, "value", {
+                    get() {
+                        return widgetValue;
+                    },
+                    set(newVal) {
+                        if (newVal !== widgetValue) {
+                            widgetValue = newVal;
+                            changeInputVisibility(node, newVal);
+                        }
+                    },
+                });
             }
-          }
-        });
-      }
-    }
-  }
+        }
+    },
 });
